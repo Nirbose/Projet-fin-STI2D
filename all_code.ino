dfbff1ef9147f80b31b882f6a96eb67f79c6fe1d
Nirbose <62440475+Nirbose@users.noreply.github.com>
2022-04-14 16:07:15 +0200

    up

--- all_code.ino
+++ all_code.ino
@@ -1,6 +1,11 @@
 #include "Seeed_LDC1612.h"
+#include <SoftwareSerial.h>
 
 LDC1612 sensor;
+
+SoftwareSerial bluetooth(8, 9); // Ne pas utiliser le 8 en Rx (Rx / Tx)
+
+String message;
 
 const u32 Vmin = 61391000;
 const u32 Vmax = 79337742;
@@ -19,16 +24,13 @@ const int LEFT = 1;
 //permet de rÃ©gler la vitesse 0=arrÃ©tÃ© et 255= vitesse max
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  bluetooth.begin(9600);
   // met les broches de 3 Ã  9 en sortie
-  for (int i = 3; i < 9; i++)
-    pinMode(i, OUTPUT);
-  // met les broches de 11 Ã  13 en sortie
-  for (int i = 11; i < 13; i++)
-    pinMode(i, OUTPUT);
-
-  pinMode(0, INPUT);
-  pinMode(1, INPUT);
+  pinMode(E1, OUTPUT);
+  pinMode(E3, OUTPUT);
+  pinMode(M1, OUTPUT);
+  pinMode(M3, OUTPUT);
 
   sensor.init();
 
@@ -41,56 +43,59 @@ void setup() {
 
 void loop() {
 
-  u32 result_channel1 = 0;
+  if (bluetooth.available()) {
 
-  /*shows the status of sensor.*/
-  //sensor.get_sensor_status();
+    message = bluetooth.read();
 
-  /*Get channel 0 result and parse it.*/
-  sensor.get_channel_result(CHANNEL_0, &result_channel1);
+    Serial.println(message);
+
+    u32 result_channel1 = 0;
+
+    /*shows the status of sensor.*/
+    //sensor.get_sensor_status();
+
+    /*Get channel 0 result and parse it.*/
+    sensor.get_channel_result(CHANNEL_0, &result_channel1);
 
 
-  float result = ((float)result_channel1 - (float)Vmin) / ((float)Vmax - (float)Vmin);
+    float result = ((float)result_channel1 - (float)Vmin) / ((float)Vmax - (float)Vmin);
 
-  Serial.println(result);
+    if (result >= 0.1) {
+      analogWrite(E1, 0);
+      analogWrite(E3, 0);
+      return;
+    }
 
-  if (result >= 0.1) {
-    analogWrite(E1, 0);
-    analogWrite(E3, 0);
-    return;
-  }
+    if (message.toInt() == 53) { // Stop btn
+      analogWrite(E1, 0);
+      analogWrite(E3, 0);
+    } else if (message.toInt() == 49) { // Avancée
 
-  int VD1 = map(analogRead(1), 0, 1023, 255, 0); // Avant Arrière
-  int VD2 = map(analogRead(0), 0, 1023, 255, 0); // Droite, Gauche
+      int speed = 255 / 2;
+      d(1, speed);
 
-  Serial.println(VD1);
+    } else if (message.toInt() == 52) { // Reculer
 
-  if (VD1 >= 115 && VD1 <= 135) {
-    analogWrite(E1, 0);
-    analogWrite(E3, 0);
-  } else if (VD1 > 135) {
+      int speed = 255 / 2;
+      d(0, speed);
 
-    // int speed = map(VD2, 135, 1023, 0, 255);
-    int speed = 255 / 2;
-    d(1, speed);
+    }
 
+    if (message.toInt() == 50) {
+      turn(RIGHT);
+
+    } else if (message.toInt() == 51) {
+      turn(LEFT);
+
+    }
+    
   } else {
-
-    // int speed = map(VD2, 0, 115, 255, 0);
-    int speed = 255 / 2;
-    d(0, speed);
-
+    
+    Serial.println("non");  
   }
 
-  if (VD2 > 135) {
-    turn(RIGHT);
-
-  } else if (VD2 < 115) {
-    turn(LEFT);
-
-  }
-
-  delay(500);
+  delay(300);
+  
 }
 
 void d(int d, int speed)
